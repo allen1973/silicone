ddc43cbed1a457e73b0c9f1060a1c5a9acd02fa3
FatGeek <yuhrenhuang@gmail.com>
2017-02-18 01:40:17 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,20 @@ var gulp = require("gulp"),//http://gulpjs.com/
 var sassFiles = "src/**/*.scss";
 var entryFile = "src/main.scss";
 
+
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var htmlReplace = require('gulp-html-replace');
+var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+var cssmin = require('gulp-cssmin');
+var browserSync = require('browser-sync');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
+var csslint = require('gulp-csslint');
+var less = require('gulp-less');
+
 gulp.task("sass", function(){
 	log("Generate CSS files " + (new Date()).toString());
 	gulp.src(entryFile)
@@ -34,3 +48,80 @@ gulp.task("watch", function(){
 });
 
 gulp.task("default", ["sass", "watch"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gulp.task('default', ['copy'], function() {
+    gulp.start(['build-img', 'usemin']);
+});
+
+gulp.task('copy', ['clean'], function() {
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', function() {
+    gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+    gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            js : [uglify],
+            css : [autoprefixer, cssmin]
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function() {
+    browserSync.init({
+        server : {
+            baseDir : 'src'
+        }
+    });
+
+    gulp.watch('src/js/**/*.js').on('change', function(event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+
+    gulp.watch('src/css/**/*.css').on('change', function(event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.reporter());
+    });
+
+
+    gulp.watch('src/less/**/*.less').on('change', function(event) {
+        gulp.src(event.path)
+            .pipe(less().on('error', function(error) {
+                console.log('LESS, erro compilação: ' + error.filename);
+                console.log(error.message);
+            }))
+            .pipe(gulp.dest('src/css'));
+    });
+
+    gulp.watch('src/**/*').on('change', browserSync.reload);
+});
+
